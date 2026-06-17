f0c1da31b3ea9cbb4b0b0087619b4ac567f0cc98
ZahinnAhmed <zahinfaiza.ahmed@gmail.com>
2026-06-17 19:36:48 -0400

    feat: add dashboard page layout

---
+++ client/src/components/PageLayoutDashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const PageLayoutDashboard = ({ children, onBack, userName, userRole, userPhoto }) => {
+  return (
+    <div className="h-screen flex flex-col bg-[#f6f6f6] overflow-hidden">
+      <Navbar userName={userName} userRole={userRole} userPhoto={userPhoto} />
+      <div className="flex-1 flex flex-col w-full px-4 py-3 overflow-hidden">
+        {children}
+      </div>
+        <button className="bg-[#003F55] text-white px-6 py-2 rounded font-bold shadow mx-12 mb-4 w-auto self-end">
+          Get Help
+        </button>
+      
+    </div>
+  )
+}
+export default PageLayoutDashboard
