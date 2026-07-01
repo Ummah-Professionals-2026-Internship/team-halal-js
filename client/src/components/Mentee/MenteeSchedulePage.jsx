035e7329cc1cf72130acb10f322cdbc658761bb7
ZahinnAhmed <zahinfaiza.ahmed@gmail.com>
2026-07-01 17:53:45 -0400

    displays mentor availability on mentee schedule page
    It fetches mentor's saved availability slots from db and passes them to the AvailabilityPick component. Now, mentee can see the
    available times of the selcted mentor. It's read only currently.

--- client/src/components/Mentee/MenteeSchedulePage.jsx
+++ client/src/components/Mentee/MenteeSchedulePage.jsx
@@ -13,6 +13,10 @@ const MenteeSchedulePage = () => {
   const navigate = useNavigate();
   const mentor = state?.mentor;
   const mentorName = mentor ? `${mentor.firstName} ${mentor.lastName}` : 'Mentor';
+  const mentorSlots = (mentor?.manualAvailabilitySlots||[]).map(
+    slot => `${slot.day}-${slot.startTime}`
+  )
+
 
   return (
     <PageLayoutDashboard userName={userName} userRole="Mentee" userPhoto={user.profilePicture} onBack={() => navigate(-1)}>
@@ -25,7 +29,7 @@ const MenteeSchedulePage = () => {
             {mentor && <MentorProfileCard mentor={mentor} />}
 
             <div className="flex-1 bg-[#C5DCE8] rounded-2xl p-4 flex flex-col gap-3">
-              <AvailabilityPick title={`${mentorName}'s Availability`} readOnly />
+              <AvailabilityPick title={`${mentorName}'s Availability`}  mentorSlots={mentorSlots} readOnly />
 
               <div className="text-center">
                 <p className="font-semibold text-[#00212C] text-sm">Auto-Selected Meeting Time:</p>
